3cc62fdfa49bd260d362102fa143431c7f4f02ab
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-06-23 22:08:50 +0200

    Laravel

---
+++ Dans projet Laravel/partials/navbar.blade.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="{{ route('accueil') }}">Accueil</a></li>
+    <li><a href="{{ route('contact') }}">Page de contact</a></li>
+    <li><a href="{{ route('info') }}">Page d'information</a></li>
+</ul>
