fa0a4eeaa349cf2a165c3f3be19fd1c462cd7e05
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-07-01 12:30:44 +0200

    Controlleur CRUD

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,32 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AdminController extends Controller
 {
     public function login()
     {
-        // return abort('404');
         return view(
             'log'
-
         );
     }
 
     public function admin()
     {
-        $user = [
-            'name' => $_POST['pseudo'],
-            'password' => $_POST['password']
-        ];
-        if (Auth::attempt($user)) {
-            return view(
-                'admin'
-            );
-        } else {
-            return back();
+        $articles = Article::all();
+        if (count($_POST) > 2) {
+            $user = [
+                'name' => $_POST['pseudo'],
+                'password' => $_POST['password']
+            ];
+
+            if (Auth::attempt($user)) {
+                Session::put('connected', true);
+                return view(
+                    'admin',
+                    compact('articles')
+                );
+            } else {
+                Session::put('connected', false);
+                return back();
+            }
         }
+    }
+
+    public function new()
+    {
+        if (Session::get('connected') === false) {
+            return abort('404');
+        }
+        if (count($_POST) > 3) {
+            Article::create([
+                'title' => $_POST['title'],
+                'image' => $_POST['image'],
+                'content' => $_POST['content'],
+                'link' => $_POST['link'],
+            ]);
+        }
+        return view(
+            'action'
+        )->with('action', 'new');
+    }
+    public function edit($id, Request $request)
+    {
+        if (Session::get('connected') === false) {
+            return abort('404');
+        }
+        $article = Article::find($id);
+
+        if (count($_POST) > 3) {
+            $update = Article::find($id);
+
+            $update->title = $request->input("title");
+            $update->image = $request->input("image");
+            $update->content = $request->input("content");
+            $update->link = $request->input("link");
+            $update->save();
+        }
+        return view(
+            'action'
+        )->with('action', 'edit')->with('article', $article);
+    }
+    public function delete($id)
+    {
+        if (Session::get('connected') === false) {
+            return abort('404');
+        }
+        $delete = Article::find($id);
+        $delete->delete();
+        return view(
+            'action'
+        )->with('action', 'delete');
     }
 }
