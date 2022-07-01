a5d92fa88e2a7655a2630276c84bbd19cedcedb9
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-07-01 12:31:22 +0200

    Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory; 
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'image',
+        'content',
+        'link',
+    ];
 }
