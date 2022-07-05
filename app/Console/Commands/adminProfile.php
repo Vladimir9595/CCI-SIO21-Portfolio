ca32b5618c7853d66c8bed82c59be5d0e8976ca2
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-07-05 22:04:55 +0200

    Commande à jour

--- app/Console/Commands/adminProfile.php
+++ app/Console/Commands/adminProfile.php
@@ -5,21 +5,21 @@ namespace App\Console\Commands;
 use App\Models\User;
 use Illuminate\Console\Command;
 
-class AdminAccount extends Command
+class adminProfile extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'admin:account {id}';
+    protected $signature = 'admin:profil {id}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Check the admin account informations';
+    protected $description = 'Search the admin profil';
 
     /**
      * Execute the console command.
