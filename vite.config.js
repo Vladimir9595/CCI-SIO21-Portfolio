fb01db322e06ac2b6024a6f27b5ca5287fa270da
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-13 16:07:44 +0100

    update vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            "resources/js/app.js",
+            "resources/scss/app.scss",
+            "resources/scss/stylePort.scss",
+            "resources/scss/styleLegal.scss",
+            "resources/scss/styleArticle.scss",
+            "resources/scss/styleAdmin.scss",
+        ]),
+    ],
+});
