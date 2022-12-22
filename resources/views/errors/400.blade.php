56b082765fa0e6753d729c030e1c13bdb6ab0339
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-22 01:07:48 +0100

    fix: add error page 400

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.errors')
+
+@section('title')
+Bad Request
+@endsection
+@section('error_title')
+<h1 id="menth1">Bad Request</h1>
+@endsection
+@section('error_type')
+400
+@endsection
+@section('error_content')
+The syntax of the request is wrong
+@endsection
