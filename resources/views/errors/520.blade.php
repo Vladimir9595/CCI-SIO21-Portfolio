222e2d659b11da2a7b80803fa62d8d173f8e7e53
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-22 01:08:02 +0100

    fix: add error page 520

---
+++ resources/views/errors/520.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.errors')
+
+@section('title')
+Unknown Error
+@endsection
+@section('error_title')
+<h1 id="menth1">Unknown Error</h1>
+@endsection
+@section('error_type')
+520
+@endsection
+@section('error_content')
+The server encountered an unknown error
+@endsection
