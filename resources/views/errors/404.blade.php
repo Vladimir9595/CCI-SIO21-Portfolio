5488955c3f577b585cd63821e05e36fc8450e335
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-22 01:09:18 +0100

    fix: update error page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,45 +1,14 @@
-@extends('layouts.app')
-@section('css')
-@vite('resources/scss/stylePort.scss')
-@vite('resources/scss/errors.scss')
-@endsection
-
+@extends('layouts.errors')
 
 @section('title')
 Page Not Found
 @endsection
-
-
-
-<body class="antialiased">
-@section('content')
-@section('titreH1')
-      <h1 id="menth1">Page Not Found</h1>
+@section('error_title')
+<h1 id="menth1">Page Not Found</h1>
 @endsection
-    <nav id="menu3">
-        <ul>
-            <li id="veilletech2">
-                <a href="/" class="lx"><strong>Accueil</strong></a>
-            </li>
-            <li id="veilletech2">
-                <a href="/veille-technologique"><strong>Veille Technologique</strong></a>
-            </li>
-            <li id="veilletech2">
-                <a href="/projets" class="dx"><strong>Projets</strong></a>
-            </li>
-        </ul>
-    </nav>
-
-        <div class="relative flex items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center sm:pt-0">
-            <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-                <div class="items-center pt-8 sm:justify-start sm:pt-0">
-                    <div class="number">
-                        404                    </div>
-
-                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                        We are sorry but this page does Not exists                    </div>
-                </div>
-            </div>
-        </div>
+@section('error_type')
+404
 @endsection
-</body>
+@section('error_content')
+We are sorry but this page does not exists
+@endsection
