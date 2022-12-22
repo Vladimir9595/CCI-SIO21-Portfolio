84ce72ba06f91227ceda50c46101b3725057f60b
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-22 01:08:40 +0100

    fix: add error page 401

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.errors')
+
+@section('title')
+Unauthorized
+@endsection
+@section('error_title')
+<h1 id="menth1">Unauthorized</h1>
+@endsection
+@section('error_type')
+401
+@endsection
+@section('error_content')
+Authentication is required to access the resource
+@endsection
