8ef8f225d268de997c40c7aebdb2344a7ae2c304
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-22 01:09:28 +0100

    fix: update error page 405

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -1,45 +1,14 @@
-@extends('layouts.app')
-@section('css')
-@vite('resources/scss/stylePort.scss')
-@vite('resources/scss/errors.scss')
-@endsection
-
+@extends('layouts.errors')
 
 @section('title')
-Page Expired
+Method Not Allowed
 @endsection
-
-
-
-<body class="antialiased">
-@section('content')
-@section('titreH1')
-      <h1 id="menth1">Page Expired</h1>
+@section('error_title')
+<h1 id="menth1">Method Not Allowed</h1>
 @endsection
-        <nav id="menu3">
-        <ul>
-            <li id="veilletech2">
-                <a href="/" class="lx"><strong>Accueil</strong></a>
-            </li>
-            <li id="veilletech2">
-                <a href="/veille-technologique"><strong>Veille Technologique</strong></a>
-            </li>
-            <li id="veilletech2">
-                <a href="/projets" class="dx"><strong>Projets</strong></a>
-            </li>
-        </ul>
-    </nav>
-
-        <div class="relative flex items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center sm:pt-0">
-            <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <div class="number">
-                        405                    </div>
-
-                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                        Sorry you do not have the permission to visit this page                    </div>
-                </div>
-            </div>
-        </div>
+@section('error_type')
+405
 @endsection
-</body>
+@section('error_content')
+Sorry you do not have the permission to visit this page
+@endsection
