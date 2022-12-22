625d8d4153534026ca3ca22d2dcc1bdfd3b8b04e
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-22 01:08:23 +0100

    fix: add error page 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.errors')
+
+@section('title')
+Service Unavailable
+@endsection
+@section('error_title')
+<h1 id="menth1">Service Unavailable</h1>
+@endsection
+@section('error_type')
+503
+@endsection
+@section('error_content')
+Service temporarily unavailable or under maintenance
+@endsection
