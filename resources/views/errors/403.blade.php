8801190ccfc9b3677014b5f3ca80c373abd79b9f
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-22 01:08:31 +0100

    fix: add error page 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.errors')
+
+@section('title')
+Forbidden
+@endsection
+@section('error_title')
+<h1 id="menth1">Forbidden</h1>
+@endsection
+@section('error_type')
+403
+@endsection
+@section('error_content')
+You can not access the resource
+@endsection
