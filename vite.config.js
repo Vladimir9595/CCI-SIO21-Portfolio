20705806c33ca4549dd5a087c0508d4b7801b019
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-02 18:47:45 +0100

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     ],
     // server: {
     //     hmr: {
-    //         host: "localhost",
-    //     }
-    // }
+    //         host: "devslam.local",
+    //     },
+    // },
 });
