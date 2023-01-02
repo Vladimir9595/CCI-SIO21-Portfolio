96fbb071237058f1ab6fc7ce1660c0eb7fca603c
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-02 18:50:54 +0100

    add 419 error page

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.errors')
+
+@section('title')
+Page Expired
+@endsection
+@section('error_title')
+<h1 id="menth1">Page Expired</h1>
+@endsection
+@section('error_type')
+419
+@endsection
+@section('error_content')
+Sorry the page takes too long to send its request
+@endsection
