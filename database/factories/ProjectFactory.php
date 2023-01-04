886d9bac209c2c6d9ea55cefb979842bf49da53f
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-04 14:59:24 +0100

    fix: Add articles et projets factory #23

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'image' => $this->faker->imageUrl(640, 480, 'cats', true),
+            'content' => $this->faker->paragraph,
+            'link' => $this->faker->url,
+            'created_at' => now()
+        ];
+    }
+}
