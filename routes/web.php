2e098728a7c85e580514d1c0e19f19d63d5b876e
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-04 13:59:31 +0100

    fix #18: update route web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MonController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +23,29 @@ use App\Http\Controllers\AdminController;
 Route::get('/', [MonController::class, 'index'])->name('accueil');
 Route::get('/mentions-légales', [MonController::class, 'mention'])->name('mentions');
 Route::get('/veille-technologique', [MonController::class, 'articles'])->name('articles');
+Route::get('/mes-projets', [MonController::class, 'projects'])->name('projects');
 
-Route::get('/login', [AdminController::class, 'login'])->name('login');
-Route::post('/admin', [AdminController::class, 'admin'])->name('admin');
-Route::post('/admin/new', [AdminController::class, 'new'])->name('new');
-Route::post('/admin/edit/{id}', [AdminController::class, 'edit'])->name('edit');
-Route::post('/admin/delete/{id}', [AdminController::class, 'delete'])->name('delete');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'index'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+});
+
+Route::prefix('admin')->middleware('auth')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::resources([
+        'article' => ArticleController::class,
+        'project' => ProjectController::class
+    ]);
+});
+
+
+
+
+// Route::post('/admin', [AdminController::class, 'admin'])->name('admin');
+// Route::post('/admin/new', [AdminController::class, 'new'])->name('new');
+// Route::post('/admin/edit/{id}', [AdminController::class, 'edit'])->name('edit');
+// Route::post('/admin/delete/{id}', [AdminController::class, 'delete'])->name('delete');
 
 // Route::get('/posts/{id}', [MonController::class, 'show'])->name('id');
 // Route::get('/contactez-nous', [MonController::class, 'contact'])->name('contact');
