c8f0161814b1bb07bf5c8ee66d61d9f1094a43ff
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-04 15:03:55 +0100

    fix: create Article controller (#21)

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('article.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'image' => 'required',
+            'content' => 'required',
+            'link' => 'required'
+        ]);
+        Article::create($request->only('title', 'image', 'content', 'link'));
+        return redirect()->route('admin')->with('message', 'Article créé avec succès');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('article.edit')->with('article', Article::find($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'image' => 'required',
+            'content' => 'required',
+            'link' => 'required'
+        ]);
+        $article = Article::find($id);
+        $article->update($request->only('title', 'image', 'content', 'link'));
+        return redirect()->route('admin')->with('message', 'Article modifié avec succès');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Article::destroy($id);
+        return redirect()->route('admin')->with('message', 'Article #' . $id . ' supprimé avec succès');
+    }
+}
