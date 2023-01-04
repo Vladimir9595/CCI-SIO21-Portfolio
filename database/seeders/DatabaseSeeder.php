bcd0ad3bb3853beee41f233a890c6a01ae442e20
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-04 14:58:11 +0100

    fix: update DatabaseSeeder (#24)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Article;
+use App\Models\Project;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Vladimir',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+        Project::factory(10)->create();
+        Article::factory(10)->create();
     }
 }
