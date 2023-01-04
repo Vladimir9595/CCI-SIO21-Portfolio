f1456ca3513756fdcb62c3647defe37569a737ed
Vladimir9595 <vladisac@hotmail.it>
2023-01-04 17:07:17 +0100

    restore project files

---
+++ app/Console/Commands/adminProfile.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class adminProfile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'admin:profil {id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Search the admin profil';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        if (User::find($this->argument()['id'])) {
+
+
+            $user = User::find($this->argument()['id']);
+
+            echo "\n";
+            echo " \e[1;42m                                                          \e[0m \n";
+            echo " \e[1;42m      Good news, we found the user in the database !      \e[0m \n";
+            echo " \e[1;42m                                                          \e[0m \n";
+
+            echo "\n  ";
+            echo "   Username => \e[1;33m " . $user->name . " \e[0m\n  ";
+            echo "      Email => \e[1;33m " . $user->email . " \e[0m\n  ";
+            echo "Email Verfied => \e[1;33m " . $user->email_verified_at . " \e[0m\n  ";
+            echo "   Password => \e[1;33m " . $user->password . " \e[0m\n  ";
+            echo "  Created at => \e[1;33m " . $user->created_at . " \e[0m\n ";
+            echo "   Updated at => \e[1;33m " . $user->updated_at . " \e[0m\n ";
+            echo "\n";
+        } else {
+
+            echo "\e[1;41m                                          \e[0m \n";
+            echo "\e[1;41m      Specified user doesn't exist...     \e[0m \n";
+            echo "\e[1;41m                                          \e[0m \n";
+        }
+    }
+}
