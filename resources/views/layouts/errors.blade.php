5a535b4f75d9a5563b3d3aa7ad9be8f2682fb15a
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-01-05 14:13:04 +0100

    fix: dark scheme (#31)

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -30,7 +30,7 @@
             </ul>
         </nav>
 
-        <div class="relative flex items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="items-center pt-8 sm:justify-start sm:pt-0">
                     <div class="number">
